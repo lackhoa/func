08d9abe3c28c0171001356039a5cc1b3069a129e
lackhoa <vodangkhoa873@gmail.com>
2018-10-04 20:36:11 +0300

    Heavy duty refactoring and expansion in sight

--- main.rkt
+++ main.rkt
@@ -7,71 +7,85 @@
                  (prefix-out old: -)
                  (prefix-out old: *)
                  (prefix-out old: /)
-                 (prefix-out old: not)))
+                 (prefix-out old: not)
+                 (prefix-out old: cond)))
 (require "lang/kara.rkt"
          'old)
 (provide (all-defined-out))
 
-;;; Functional Forms
-(def ((construct . fs) x)
-  (let/ec escape
-    (and (list? fs)
-       x
-       (for/list ([f  fs])
-         (match (f x)
-           [#f  (escape #f)]
-           [fx  fx])))))
+(def None 'None  #|The Divine error code|#)
 
-(define-syntax condition
+;;; Sequence-forming Forms
+;;; These are all macross since we don't want eager evaluation
+(define-syntax-rule (cons x seq)
+  (match x
+    ['None  None]
+    [_      (match seq
+              ['None  None]
+              [_      (old:cons x seq)])]))
+
+(define-syntax list
   (syntax-rules ()
-    [(_ [p1 f1] [p2 f2] ... g)
-     (lam (x)
-       (let/ec escape
-         (cond [(match (p1 x)
-                  ['T  #t]
-                  ['F  #f]
-                  [_   (escape #f)])  (f1 x)]
-               [(match (p2 x)
-                  ['T  #t]
-                  ['F  #f]
-                  [_   (escape #f)])  (f2 x)]
-               ...
-               [else  (g x)])))]))
+    [(_)            null]
+    [(_ x1 x2 ...)  (cons x1 (list x2 ...))]))
 
-(def ((const x) y)
-  (and y x))
+;;; Functional Forms
+(def ((a . fs) x)
+  ;; Construct
+  (let loop ([res  null]
+             [fs   fs])
+    (match fs
+      ['()          (old:reverse res)]
+      [(cons f fs)  (match (f x)
+                      ['None  'None]
+                      [fx     (loop (cons fx res)
+                                    fs)])])))
 
-(def ((insert f default) xs)
-  ;; Reduce/foldl
+(define-syntax-rule (cond [p f]
+                          ...
+                          g)
+  ;; Like condition, but if the predicate doesn#t
+  ;; return a boolean then fuck it
+  (lam (x)
+    (let/ec escape
+      (old:cond [(match (p x)
+                   [#t  #t]
+                   [#f  #f]
+                   [_   (escape None)])  (f x)]
+                ...
+                [else                    (g x)]))))
+
+(def (fuck-none f)
+  (lam (x)
+    (match x
+      ['None  'None]
+      [_      (f x)])))
+
+(def (k x)
+  ;; Constant
+  (fuck-none (lam (_) x)))
+
+(def ((// f default) xs)
+  ;; Reduce/foldl (official name being 'insert')
   (match xs
     ['()               default]
     [(cons xcar xcdr)  (f (list xcar
-                                ((insert f default) xcdr)))]
-    [_                 #f]))
+                                ((// f default) xcdr)))]
+    [_                 None]))
 
-(def ((alpha f) xs)
+(def ((al f) xs)
   ;; Apply to all
-  (and (list? xs)
-     (map f xs)))
+  (if (list? xs) (map f xs) None))
 
 (def ((bu f x) y)
   ;; Binary to unary (currying)
-  (and x
-     (f `(,x ,y))))
+  (f (cons x (cons y null)  #|Done like this to avoid invalid list|#)))
 
 (def ((while p f) x)
   (match (p x)
-    ['T  ((while p f) (f x))]
-    ['F  x]
-    [_   #f]))
-
-;; #f stands for the bottom value, 'T and 'F stand for true and false
-(def (bsym b)  (match b [#t  'T] [#f  'F]))
-(def (rbsym s) (match s ['T  #t] ['F  #f]))
-
-(def (cons x seq)
-  ;; Sequence-forming
-  (and x (old:cons x seq)))
+    [#t  ((while p f) (f x))]
+    [#f  x]
+    [_   None]))
 
 (def ((seq-ref pos) seq)
   ;; Sequence selector
@@ -83,6 +97,15 @@
   (and (list? seq) (cdr seq)))
 
 (def id identity)
+
+(def (show x)
+  ;; The eye of God
+  (begin (displayln x)
+         (identity x)))
+
+(def (comshow . fs)
+  ;; The debugger of God
+  (apply compose (add-between fs show)))
 
 ;;; Booleans
 (def (atom x)
@@ -156,24 +179,24 @@
 
 (def (& xy)
   (match xy
-    [(list 'T 'T)  'T]
-    [(list 'T 'F)  'F]
-    [(list 'F 'T)  'F]
-    [(list 'F 'F)  'F]
+    [(list #t #t)  #t]
+    [(list #t #f)  #f]
+    [(list #f #t)  #f]
+    [(list #f #f)  #f]
     [_             #f]))
 
 (def (v xy)
   (match xy
-    [(list 'T 'T)  'T]
-    [(list 'T 'F)  'T]
-    [(list 'F 'T)  'T]
-    [(list 'F 'F)  'F]
+    [(list #t #t)  #t]
+    [(list #t #f)  #t]
+    [(list #f #t)  #t]
+    [(list #f #f)  #f]
     [_             #f]))
 
 (def (not x)
   (match x
-    ['T  'F]
-    ['F  'T]
+    [#t  #f]
+    [#f  #t]
     [_   #f]))
 
 (def (apndl x-ys)
@@ -195,14 +218,13 @@
   (compose (seq-ref pos) reverse))
 
 (def rotl
-  (compose apndr (construct tl
-                      (seq-ref 0))))
+  (compose apndr (a tl
+              (seq-ref 0))))
 
 (def rotr
-  (compose apndl (construct (seq-refr 0)
-                      tlr)))
+  (compose apndl (a (seq-refr 0)
+              tlr)))
 
 (def last
-  (condition
-   [(compose null tl)  (seq-ref 0)]
-   (compose last tl)))
+  (cond [(compose null tl)  (seq-ref 0)]
+        (compose last tl)))
--- test.rkt
+++ test.rkt
@@ -3,37 +3,25 @@
          "main.rkt")
 
 (def !
-  (let ([eq0   (compose eq (construct id
-                                (const 0)))]
-        [sub1  (compose - (construct id
-                               (const 1)))])
-    (condition
-     [eq0  (const 1)]
-     (compose * (construct id
-                     (compose ! sub1))))))
+  (let ([eq0   (compose eq (A id (K 0)))]
+        [sub1  (compose - (A id (K 1)))])
+    (cond [eq0  (K 1)]
+          (compose * (A id (compose ! sub1))))))
 (! 5)
 
 (def dot-prod
-  (compose (insert + 0) (alpha *) trans))
+  (compose (// + 0) (al *) trans))
 
 (dot-prod '([1 2 3] [12 6 4]))
 
 (def mat-mult
   ;; Argument of the form <m, n>
-  (compose (alpha (alpha dot-prod))
-     (alpha distl)
+  (compose (al (al dot-prod))
+     (al distl)
      distr
-     (construct (seq-ref 0  #|m|#)
-                (compose trans (seq-ref 1)  #|trans n|#))))
+     (A (seq-ref 0  #|m|#)
+        (compose trans (seq-ref 1)  #|trans n|#))))
 
 (let ([test-pair '(([1 2 3] [4 5 6])
-                   ([7 8] [9 10] [11 12]))]
-      [step1     (construct (seq-ref 0  #|m|#)
-                            (compose trans (seq-ref 1)  #|trans n|#))]
-      [step2     distr]
-      [step3     (alpha distl)]
-      [step4     (alpha (alpha dot-prod))])
-  (displayln (step1 test-pair))
-  (displayln ((compose step2 step1) test-pair))
-  (displayln ((compose step3 step2 step1) test-pair))
-  (displayln ((compose step4 step3 step2 step1) test-pair)))
+                   ([7 8] [9 10] [11 12]))])
+  (displayln (mat-mult test-pair)))
