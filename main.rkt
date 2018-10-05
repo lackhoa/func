5dda8556f90e4ec3ef05134353b0fb13ad31c6ae
lackhoa <vodangkhoa873@gmail.com>
2018-10-05 17:51:41 +0300

    Fixed a lot of naming and code

--- main.rkt
+++ main.rkt
@@ -3,42 +3,52 @@
         (provide (prefix-out old: cons)
                  (prefix-out old: length)
                  (prefix-out old: reverse)
+                 (prefix-out old: eq?)
                  (prefix-out old: +)
                  (prefix-out old: -)
                  (prefix-out old: *)
                  (prefix-out old: /)
                  (prefix-out old: not)
-                 (prefix-out old: cond)))
+                 (prefix-out old: cond)
+                 (prefix-out old: list-ref)
+                 (prefix-out old: list?)
+                 (prefix-out old: list)
+                 (prefix-out old: cdr)
+                 (prefix-out old: map)
+                 (prefix-out old: drop-right)))
 (require "lang/kara.rkt"
          'old)
 (provide (all-defined-out))
 
 (def None 'None  #|The Divine error code|#)
 
-;;; Sequence-forming Forms
-;;; These are all macross since we don't want eager evaluation
-(define-syntax-rule (cons x seq)
+;;; Sequence-forming Forms, to protect list from containing None
+(def (cons x seq)
   (match x
     ['None  None]
-    [_      (match seq
-              ['None  None]
-              [_      (old:cons x seq)])]))
+    [_      (if (list? seq)
+                (old:cons x seq)
+                None)]))
 
 (define-syntax list
+  ;; This is a macro, since we don't want eagerness,
+  ;; and it's just a meta-function
   (syntax-rules ()
     [(_)            null]
-    [(_ x1 x2 ...)  (cons x1 (list x2 ...))]))
+    [(_ x1 x2 ...)  (match x1
+                      ['None  None]
+                      [_      (cons x1 (list x2 ...))])]))
 
 ;;; Functional Forms
-(def ((a . fs) x)
-  ;; Construct
+(def ((pam . fs) x)
+  ;; Construct (the opposite of `map`)
   (let loop ([res  null]
              [fs   fs])
     (match fs
       ['()          (old:reverse res)]
       [(cons f fs)  (match (f x)
-                      ['None  'None]
-                      [fx     (loop (cons fx res)
+                      ['None  None]
+                      [fx     (loop (old:cons fx res)
                                     fs)])])))
 
 (define-syntax-rule (cond [p f]
@@ -55,31 +65,35 @@
                 ...
                 [else                    (g x)]))))
 
-(def (fuck-none f)
-  (lam (x)
-    (match x
-      ['None  'None]
-      [_      (f x)])))
-
-(def (k x)
+(def ((k x) y)
   ;; Constant
-  (fuck-none (lam (_) x)))
+  (match y
+    ['None None]
+    [_     x]))
 
 (def ((// f default) xs)
-  ;; Reduce/foldl (official name being 'insert')
+  ;; Reduce/foldl (official name 'insert')
   (match xs
     ['()               default]
     [(cons xcar xcdr)  (f (list xcar
                                 ((// f default) xcdr)))]
     [_                 None]))
 
-(def ((al f) xs)
+(def ((map f) xs)
   ;; Apply to all
-  (if (list? xs) (map f xs) None))
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (let loop ([res  null]
+               [xs   xs])
+      (match xs
+        ['()               (old:reverse res)]
+        [(cons xcar xcdr)  (match (f xcar)
+                             ['None  None]
+                             [fxcar  (loop (old:cons fxcar res)
+                                           xcdr)])]))))
 
 (def ((bu f x) y)
   ;; Binary to unary (currying)
-  (f (cons x (cons y null)  #|Done like this to avoid invalid list|#)))
+  (f (cons x (cons y null)  #|Done to avoid invalid list|#)))
 
 (def ((while p f) x)
   (match (p x)
@@ -87,14 +101,18 @@
     [#f  x]
     [_   None]))
 
-(def ((seq-ref pos) seq)
-  ;; Sequence selector
-  (and (< pos (length seq))
-     (list-ref seq pos)))
+(def ((list-ref pos) seq)
+  ;; List selector (kind of like python)
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (match pos
+      [(or (? positive?) 0)  (old:list-ref seq pos)]
+      [_                    (old:list-ref (old:reverse seq)
+                                          (sub1 (- pos)))])))
 
-(def (tl seq)
+(def (cdr seq)
   ;; cdr
-  (and (list? seq) (cdr seq)))
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (old:cdr seq)))
 
 (def id identity)
 
@@ -108,74 +126,80 @@
   (apply compose (add-between fs show)))
 
 ;;; Booleans
-(def (atom x)
-  (and x (compose bsym old:not list?)))
+(def (list? x)
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (old:list? x)))
 
-(def (eq pair)
-  (and pair
-     (match pair
-       [(list x y)  (bsym (eq? x y))]
-       [_           #f])))
+(def (eq? xy)
+  (match xy
+    [(list x y)  (old:eq? x y)]
+    [_           'None]))
 
-(def (null seq)
-  (and seq
-     (bsym (null? seq))))
+(def (null? x)
+  (match x
+    ['None  None]
+    ['()    #t]
+    [_      #f]))
 
 (def (reverse seq)
-  (and (list? seq)
-     (old:reverse seq)))
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (old:reverse seq)))
 
 (def (distl x-ys)
-  (match x-ys
-    [(list x (? list? ys))  (for/list ([y  ys])
-                              (list x y))]
-    [_                      #f]))
+  (let/ec escape
+    (match x-ys
+      [(list 'None _)         None]
+      [(list x (? list? ys))  (for/list ([y  ys])
+                                (match y
+                                  ['None  (escape None)]
+                                  [_      (list x y)]))]
+      [_                      None])))
 
 (def (distr xs-y)
-  (match xs-y
-    [(list (? list? xs) y)  (for/list ([x  xs])
-                              (list x y))]
-    [_                      #f]))
+  (let/ec escape
+    (match xs-y
+      [(list _ 'None)         None]
+      [(list (? list? xs) y)  (for/list ([x  xs])
+                                (match x
+                                  ['None  (escape None)]
+                                  [_      (list x y)]))]
+      [_                      None])))
 
 (def (length ls)
-  (and (list? ls)
-     (old:length ls)))
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (old:length ls)))
 
 ;;; Arightmetic
 (def (+ xy)
   (match xy
-    [(list x y)  (and (number? x)
-                    (number? y)
-                    (old:+ x y))]
-    [_           #f]))
+    [(list x y)  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+                   (old:+ x y))]
+    [_           None]))
 
 (def (- xy)
   (match xy
-    [(list x y)  (and (number? x)
-                    (number? y)
-                    (old:- x y))]
-    [_           #f]))
+    [(list x y)  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+                   (old:- x y))]
+    [_           None]))
 
 (def (* xy)
   (match xy
-    [(list x y)  (and (number? x)
-                    (number? y)
-                    (old:* x y))]
-    [_           #f]))
+    [(list x y)  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+                   (old:* x y))]
+    [_           None]))
 
 (def (/ xy)
   (match xy
-    [(list x y)  (and (number? x)
-                    (number? y)
-                    (old:not (eq? y 0))
-                    (old:/ x y))]
-    [_           #f]))
+    [(list x y)  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+                   (old:/ x y))]
+    [_           None]))
 
 (def (trans xss)
-  (and (list? xss)
-     (forall? list? xss)
-     (apply eq*? (map length xss))
-     (apply map list xss)))
+  ;; Transpose
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (match (apply eq*? (old:map old:length xss))
+      [#f  None]
+      [#t  (apply old:map old:list xss)])))
 
 (def (& xy)
   (match xy
@@ -183,7 +207,7 @@
     [(list #t #f)  #f]
     [(list #f #t)  #f]
     [(list #f #f)  #f]
-    [_             #f]))
+    [_             None]))
 
 (def (v xy)
   (match xy
@@ -191,40 +215,35 @@
     [(list #t #f)  #t]
     [(list #f #t)  #t]
     [(list #f #f)  #f]
-    [_             #f]))
+    [_             None]))
 
 (def (not x)
   (match x
     [#t  #f]
     [#f  #t]
-    [_   #f]))
+    [_   None]))
 
-(def (apndl x-ys)
-  (match x-ys
-    [(list x (? list? ys))  (cons x ys)]
-    [_                      #f]))
-
-(def (apndr xs-y)
+(def (rcons xs-y)
   (match xs-y
-    [(list (? list? xs) y)  (pad xs y)]
-    [_                      #f]))
+    [(list (? list? xs) y)  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+                              (append xs `[,y]))]
+    [_                      None]))
 
-(def (tlr xs)
-  (and (list? xs)
-     (drop-right xs 1)))
+(def ((drop-right n) ls)
+  (with-handlers ([exn:fail?  (lambda (_)  'None)])
+    (old:drop-right ls n)))
 
 ;; Derived functions
-(def (seq-refr pos)
-  (compose (seq-ref pos) reverse))
-
-(def rotl
-  (compose apndr (a tl
-              (seq-ref 0))))
-
-(def rotr
-  (compose apndl (a (seq-refr 0)
-              tlr)))
+(def rcdr
+  (drop-right 1))
 
 (def last
-  (cond [(compose null tl)  (seq-ref 0)]
-        (compose last tl)))
+  (list-ref -1))
+
+(def rotl
+  (compose rcons (pam cdr
+                (list-ref 0))))
+
+(def rotr
+  (compose cons (pam (list-ref -1)
+               rcdr)))
--- test.rkt
+++ test.rkt
@@ -3,24 +3,24 @@
          "main.rkt")
 
 (def !
-  (let ([eq0   (compose eq (A id (K 0)))]
-        [sub1  (compose - (A id (K 1)))])
-    (cond [eq0  (K 1)]
-          (compose * (A id (compose ! sub1))))))
+  (let ([eq0?   (compose eq? (pam id (k 0)))]
+        [sub1  (compose - (pam id (k 1)))])
+    (cond [eq0?  (k 1)]
+          (compose * (pam id (compose ! sub1))))))
 (! 5)
 
 (def dot-prod
-  (compose (// + 0) (al *) trans))
+  (compose (// + 0) (map *) trans))
 
 (dot-prod '([1 2 3] [12 6 4]))
 
 (def mat-mult
   ;; Argument of the form <m, n>
-  (compose (al (al dot-prod))
-     (al distl)
+  (compose (map (map dot-prod))
+     (map distl)
      distr
-     (A (seq-ref 0  #|m|#)
-        (compose trans (seq-ref 1)  #|trans n|#))))
+     (pam (list-ref 0  #|m|#)
+          (compose trans (list-ref 1)  #|trans n|#))))
 
 (let ([test-pair '(([1 2 3] [4 5 6])
                    ([7 8] [9 10] [11 12]))])
